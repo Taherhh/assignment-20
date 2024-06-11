565e1a611a3c664657564d63ad941e0b4a169322
Taher Shabbir <taherihh53@gmail.com>
2024-06-10 18:53:01 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+// assignment 20
+var countries = ["Usa", "Canada", "France", "Germany", "Japan"];
+// print list of countries
+console.log("List Of Countries:", countries.forEach(function (country) {
+    console.log(country);
+}));
---
+++ main.ts
@@ -0,0 +1,8 @@
+// assignment 20
+let countries:string[] = ["Usa", "Canada", "France", "Germany", "Japan"]
+
+// print list of countries
+console.log("List Of Countries:", countries.forEach(country => {
+    console.log(country);
+    
+}));
